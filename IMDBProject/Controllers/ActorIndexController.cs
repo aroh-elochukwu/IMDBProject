1b5c7e50869b28ebd2d459636cb04281c5e6aa4d
Frederick Lopez <mtester010@gmail.com>
2022-07-11 15:44:37 -0500

    Implemented an ActorIndex

---
+++ IMDBProject/Controllers/ActorIndexController.cs
@@ -0,0 +1,43 @@
+﻿using IMDBProject.Models;
+using IMDBProject.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMDBProject.Controllers
+{
+    public class ActorIndexController : Controller
+    {
+        
+        private IMDBContext _db { get; set; }
+
+        public ActorIndexController(IMDBContext context)
+        {
+            _db = context;
+        }
+
+        public IActionResult Index()
+        {
+            ActorSelectViewModel vm = new ActorSelectViewModel(_db.Actors.ToList());
+            return View(vm);
+        }
+
+        public IActionResult ActorDetails(int? actorId)
+        {
+            if (actorId != null)
+            {
+                try
+                {
+                    Actor actor = _db.Actors.Include(a => a.Roles).First(a => a.Id == actorId);
+                    return View(actor);
+                } catch
+                {
+                    return RedirectToAction("Error", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        }
+    }
+}
---
+++ IMDBProject/Models/ViewModels/ActorSelectViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace IMDBProject.Models.ViewModels
+{
+    public class ActorSelectViewModel
+    {
+        public List<SelectListItem> ActorSelect { get; set; }
+
+        public ActorSelectViewModel(List<Actor> actors)
+        {
+            ActorSelect = new List<SelectListItem>();
+            actors.ForEach(a =>
+            {
+                ActorSelect.Add(new SelectListItem(a.Name, a.Id.ToString()));
+            });
+        }
+    }
+}
