711c43c07a49f39c8999ffdaf6a18f84b3495f8b
Victor Olukayode Olure <victorolure@gmail.com>
2022-07-12 12:38:10 -0500

    Add Category-select

---
+++ IMDBProject/Controllers/CategoryIndexController.cs
@@ -0,0 +1,44 @@
+﻿using IMDBProject.Models;
+using IMDBProject.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMDBProject.Controllers
+{
+    public class CategoryIndexController : Controller
+    {
+        private IMDBContext _db { get; set; }
+
+        public CategoryIndexController(IMDBContext context)
+        {
+            _db = context;
+        }
+        public IActionResult Index()
+        {
+            CategorySelectViewModel vm = new CategorySelectViewModel(_db.Categories.ToList());
+            return View(vm);
+        }
+
+        public IActionResult CategoryDetails(int? categoryId)
+        {
+            if (categoryId != null)
+            {
+                try
+                {
+                    Category category = _db.Categories.Include(c => c.Movies).First(c => c.Id == categoryId);
+                    return View(category);
+                }
+                catch
+                {
+                    return RedirectToAction("Error", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+
+    }
+}
---
+++ IMDBProject/Models/ViewModels/CategorySelectViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace IMDBProject.Models.ViewModels
+{
+    public class CategorySelectViewModel
+    {
+        public List<SelectListItem> CategorySelect { get; set; }
+
+        public CategorySelectViewModel(List<Category> categories)
+        {
+            CategorySelect = new List<SelectListItem>();
+            categories.ForEach(c =>
+            {
+                CategorySelect.Add(new SelectListItem(c.Name, c.Id.ToString()));
+            });
+        }
+    }
+}
